d26f46f6503e702c9258153937f8d3864a410784
starcoex <69073390+starcoex@users.noreply.github.com>
2023-03-06 22:57:42 +0900

    react nomfilx part1

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Movies from './Components/Movies';
+import Search from './Components/Search';
+import Series from './Components/Series';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <HelmetProvider>
+        <Helmet>
+          <title>React Nomfilx</title>
+          <link href='https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap' rel='stylesheet'></link>
+        </Helmet>
+      </HelmetProvider>
+      <Header />
+      <Routes>
+        <Route path="/series" element={<Series />}>
+        </Route>
+        <Route path="/movies" element={<Movies />}>
+        </Route>
+        <Route path="/search" element={<Search />}>
+        </Route>
+        <Route path="/" element={<Home />}>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Components/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+const Container = styled.div`
+background-color:whitesmoke;
+height: 200vh;
+`
+export default function Home() {
+  return (
+    <Container>Home</Container>
+  )
+}
